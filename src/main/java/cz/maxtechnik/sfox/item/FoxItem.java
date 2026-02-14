1d7f9667cbb0606c95bb35f61bc5b97ad88aca16
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-02-14 22:48:49 +0100

    null fix

--- src/main/java/cz/maxtechnik/sfox/item/FoxItem.java
+++ src/main/java/cz/maxtechnik/sfox/item/FoxItem.java
@@ -77,12 +77,8 @@ public abstract class FoxItem extends ArmorItem {
 		public void initializeClient(Consumer<IClientItemExtensions> consumer) {
 			consumer.accept(new IClientItemExtensions() {
 				@Override
-				public HumanoidModel getHumanoidArmorModel(LivingEntity living, ItemStack stack, EquipmentSlot slot, HumanoidModel defaultModel) {
-					HumanoidModel armorModel = new HumanoidModel(new ModelPart(Collections.emptyList(),
-							Map.of("head", new FoxModel(Minecraft.getInstance().getEntityModels().bakeLayer(FoxModel.LAYER_LOCATION)).Head, "hat", new ModelPart(Collections.emptyList(), Collections.emptyMap()), "body",
-									new ModelPart(Collections.emptyList(), Collections.emptyMap()), "right_arm", new ModelPart(Collections.emptyList(), Collections.emptyMap()), "left_arm",
-									new ModelPart(Collections.emptyList(), Collections.emptyMap()), "right_leg", new ModelPart(Collections.emptyList(), Collections.emptyMap()), "left_leg",
-									new ModelPart(Collections.emptyList(), Collections.emptyMap()))));
+				public @NotNull HumanoidModel getHumanoidArmorModel(LivingEntity living,ItemStack stack,EquipmentSlot slot,HumanoidModel defaultModel) {
+					HumanoidModel armorModel = new HumanoidModel(new ModelPart(Collections.emptyList(),Map.of("head",new FoxModel(Minecraft.getInstance().getEntityModels().bakeLayer(FoxModel.LAYER_LOCATION)).Head,"hat",new ModelPart(Collections.emptyList(),Collections.emptyMap()),"body",new ModelPart(Collections.emptyList(),Collections.emptyMap()),"right_arm",new ModelPart(Collections.emptyList(),Collections.emptyMap()),"left_arm",new ModelPart(Collections.emptyList(),Collections.emptyMap()),"right_leg",new ModelPart(Collections.emptyList(),Collections.emptyMap()),"left_leg",new ModelPart(Collections.emptyList(),Collections.emptyMap()))));
 					armorModel.crouching = living.isShiftKeyDown();
 					armorModel.riding = defaultModel.riding;
 					armorModel.young = living.isBaby();
