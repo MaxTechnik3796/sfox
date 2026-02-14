fe5e22326b5bab13f5b96d83249354b4cc39bcd4
MaxTechnik <maxtechnik.oficial@gmail.com>
2026-02-14 23:48:49 +0100

    null fix 2

--- src/main/java/cz/maxtechnik/sfox/FoxProcedure.java
+++ src/main/java/cz/maxtechnik/sfox/FoxProcedure.java
@@ -10,7 +10,6 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.LevelAccessor;
@@ -20,83 +19,80 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.network.chat.Component;
 import net.minecraft.nbt.CompoundTag;
 
-import javax.annotation.Nullable;
-
+import java.util.Objects;
 @Mod.EventBusSubscriber
-public class FoxProcedure {
+public class FoxProcedure{
 	@SubscribeEvent
-	public static void onRightClickEntity(PlayerInteractEvent.EntityInteract event) {
-		if (event.getHand() != event.getEntity().getUsedItemHand())
+	public static void onRightClickEntity(PlayerInteractEvent.EntityInteract event){
+		if(event.getHand()!=event.getEntity().getUsedItemHand())
 			return;
-		pickup(event, event.getLevel(), event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getTarget(), event.getEntity());
+		pickup(event.getTarget(),event.getEntity());
 	}
-    private static void pickup(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity, Entity sourceentity) {
-        if (entity == null || sourceentity == null || !sourceentity.isShiftKeyDown() || !(ForgeRegistries.ENTITY_TYPES.getKey(entity.getType()).toString()).equals("minecraft:fox"))
-            return;
-        CompoundTag entityNBT=entity.saveWithoutId(new CompoundTag());
-        String foxType=entityNBT.getString("Type");
-        ItemStack foxItemStack;
-        if (foxType.equals("red")) {
-            foxItemStack = new ItemStack(SfoxMod.FOX.get()).copy();
-        } else if (foxType.equals("snow")) {
-            foxItemStack = new ItemStack(SfoxMod.SNOW_FOX.get()).copy();
-        } else {
-            return;
-        }
-
-        if (entityNBT.contains("CustomName") && !entityNBT.getString("CustomName").isEmpty()) {
-            foxItemStack.setHoverName(Component.Serializer.fromJson(entityNBT.getString("CustomName")));
-        }
-        if (entityNBT.contains("Age")) {
-            foxItemStack.getOrCreateTag().putInt("FoxAge", entityNBT.getInt("Age"));
-        }
-        if (sourceentity instanceof Player player) {
-            foxItemStack.setCount(1);
-            ItemHandlerHelper.giveItemToPlayer(player, foxItemStack);
-        }
-        if(entity instanceof LivingEntity livingEntity){
-            ItemStack heldItem=livingEntity.getMainHandItem();
-            if(!heldItem.isEmpty()){
-                livingEntity.spawnAtLocation(heldItem);
-            }
-        }
-        if (!entity.level().isClientSide()) {
-            entity.discard();
-        }
-    }
-    public static void place(LevelAccessor world, double x, double y, double z, Direction direction, Entity entity, ItemStack itemstack){
-        if(direction==null||entity==null)
-            return;
-        x=x+direction.getStepX()+0.5;
-        y=y+direction.getStepY();
-        z=z+direction.getStepZ()+0.5;
-        String customNBT="";
-        if(itemstack.hasCustomHoverName()){
-            String nameJson=Component.Serializer.toJson(itemstack.getHoverName());
-            customNBT+=",CustomName:" +"'"+nameJson+"'";
-        }
-        if(itemstack.hasTag()){
-            assert itemstack.getTag()!=null;
-            if (itemstack.getTag().contains("FoxAge")){
-                int age=itemstack.getTag().getInt("FoxAge");
-                customNBT+=",Age:" + age;
-            }
-        }
-        String foxType;
-        if(itemstack.getItem()==SfoxMod.FOX.get()){
-            foxType="red";
-        }else if(itemstack.getItem()==SfoxMod.SNOW_FOX.get()){
-            foxType="snow";
-        }else{
-            return;
-        }
-        if(entity instanceof Player player){
-            ItemStack stackToRemove=itemstack.copy();
-            stackToRemove.setCount(1);
-            player.getInventory().clearOrCountMatchingItems(p->stackToRemove.getItem()==p.getItem(),1,player.inventoryMenu.getCraftSlots());
-        }
-        String finalCommand = String.format("summon fox ~ ~ ~ {Type:%s%s}", foxType, customNBT);
-        if (world instanceof ServerLevel level)
-            level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL,new Vec3(x,y,z), Vec2.ZERO,level,4,"",Component.literal(""),level.getServer(),null).withSuppressedOutput(),finalCommand);
-    }
+	private static void pickup(Entity entity,Entity sourceentity){
+		if(entity==null||sourceentity==null||!sourceentity.isShiftKeyDown()||!(Objects.requireNonNull(ForgeRegistries.ENTITY_TYPES.getKey(entity.getType())).toString()).equals("minecraft:fox"))return;
+		CompoundTag entityNBT=entity.saveWithoutId(new CompoundTag());
+		String foxType=entityNBT.getString("Type");
+		ItemStack foxItemStack;
+		if(foxType.equals("red")){
+			foxItemStack=new ItemStack(SfoxMod.FOX.get()).copy();
+		}else if(foxType.equals("snow")){
+			foxItemStack=new ItemStack(SfoxMod.SNOW_FOX.get()).copy();
+		}else{
+			return;
+		}
+		if(entityNBT.contains("CustomName")&&!entityNBT.getString("CustomName").isEmpty()){
+			foxItemStack.setHoverName(Component.Serializer.fromJson(entityNBT.getString("CustomName")));
+		}
+		if(entityNBT.contains("Age")){
+			foxItemStack.getOrCreateTag().putInt("FoxAge",entityNBT.getInt("Age"));
+		}
+		if(sourceentity instanceof Player player){
+			foxItemStack.setCount(1);
+			ItemHandlerHelper.giveItemToPlayer(player,foxItemStack);
+		}
+		if(entity instanceof LivingEntity livingEntity){
+			ItemStack heldItem=livingEntity.getMainHandItem();
+			if(!heldItem.isEmpty()){
+				livingEntity.spawnAtLocation(heldItem);
+			}
+		}
+		if(!entity.level().isClientSide()){
+			entity.discard();
+		}
+	}
+	public static void place(LevelAccessor world,double x,double y,double z,Direction direction,Entity entity,ItemStack itemstack){
+		if(direction==null||entity==null)
+			return;
+		x=x+direction.getStepX()+0.5;
+		y=y+direction.getStepY();
+		z=z+direction.getStepZ()+0.5;
+		String customNBT="";
+		if(itemstack.hasCustomHoverName()){
+			String nameJson=Component.Serializer.toJson(itemstack.getHoverName());
+			customNBT+=",CustomName:"+"'"+nameJson+"'";
+		}
+		if(itemstack.hasTag()){
+			assert itemstack.getTag()!=null;
+			if(itemstack.getTag().contains("FoxAge")){
+				int age=itemstack.getTag().getInt("FoxAge");
+				customNBT+=",Age:"+age;
+			}
+		}
+		String foxType;
+		if(itemstack.getItem()==SfoxMod.FOX.get()){
+			foxType="red";
+		}else if(itemstack.getItem()==SfoxMod.SNOW_FOX.get()){
+			foxType="snow";
+		}else{
+			return;
+		}
+		if(entity instanceof Player player){
+			ItemStack stackToRemove=itemstack.copy();
+			stackToRemove.setCount(1);
+			player.getInventory().clearOrCountMatchingItems(p->stackToRemove.getItem()==p.getItem(),1,player.inventoryMenu.getCraftSlots());
+		}
+		String finalCommand=String.format("summon fox ~ ~ ~ {Type:%s%s}",foxType,customNBT);
+		if(world instanceof ServerLevel level)
+			level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL,new Vec3(x,y,z),Vec2.ZERO,level,4,"",Component.literal(""),level.getServer(),null).withSuppressedOutput(),finalCommand);
+	}
 }
